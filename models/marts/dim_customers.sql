2f70995ce5ead6ab95e4cb5fe74e2205efe13c63
joaobssa <110626797+joaobssa@users.noreply.github.com>
2026-09-09 11:00:25 +0100

    added staging models, refactored project and established materialization strategy

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,12 @@
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop_customers
+    select * from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop_orders
+    select * from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
